bc979e0e4d6fa7991d5448cd27c27d7fd2b915fa
jflorimo <flojeanc@yahoo.fr>
2015-12-11 16:30:11 +0100

    fe

---
+++ app/src/main/java/com/jflorimo/ft_hangouts/MessagesActivity.java
@@ -0,0 +1,16 @@
+package com.jflorimo.ft_hangouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jflorimo on 11/12/15.
+ */
+public class MessagesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages);
+    }
+}

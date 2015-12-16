c39c1d952403a446e2b4f270789b21ac721e5bbf
jflorimo <flojeanc@yahoo.fr>
2015-12-16 16:11:27 +0100

    efef

--- app/src/main/java/com/jflorimo/ft_hangouts/MessagesActivity.java
+++ app/src/main/java/com/jflorimo/ft_hangouts/MessagesActivity.java
@@ -1,7 +1,11 @@
 package com.jflorimo.ft_hangouts;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -16,6 +20,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 /**
  * Created by jflorimo on 11/12/15.
  */
@@ -25,6 +31,8 @@ public class MessagesActivity extends Activity {
     private ArrayAdapter<String> adapter;
     private TextView messageTextView;
     private Button sendButton;
+    private TextView loginText;
+    private TextView numberText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,21 +96,82 @@ public class MessagesActivity extends Activity {
     View.OnClickListener sendButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            closeKeyboard();
+			closeKeyboard();
+			/*
+			try {
+				SmsManager smsManager = SmsManager.getDefault();
+				smsManager.sendTextMessage("+33608421973", null,messageTextView.getText().toString(), null, null);
+				Toast.makeText(getApplicationContext(), "SMS Sent!",
+						Toast.LENGTH_LONG).show();
+			} catch (Exception e) {
+				Toast.makeText(getApplicationContext(),
+						"SMS faild, please try again later!",
+						Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+			}
+			*/
 
-            try {
-                SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage("+33608421973", null,messageTextView.getText().toString(), null, null);
-                Toast.makeText(getApplicationContext(), "SMS Sent!",
-                        Toast.LENGTH_LONG).show();
-            } catch (Exception e) {
-                Toast.makeText(getApplicationContext(),
-                        "SMS faild, please try again later!",
-                        Toast.LENGTH_LONG).show();
-                e.printStackTrace();
-            }
+			SmsManager smsManager = SmsManager.getDefault();
 
+			String phoneNumber = "9999999999";
+			String smsBody = "Some piece of really long text, longer than 140*n characters!";
 
+			String SMS_SENT = "SMS_SENT";
+			String SMS_DELIVERED = "SMS_DELIVERED";
+
+			PendingIntent sentPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent(SMS_SENT), 0);
+			PendingIntent deliveredPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent(SMS_DELIVERED), 0);
+
+			ArrayList<String> smsBodyParts = smsManager.divideMessage(smsBody);
+			ArrayList<PendingIntent> sentPendingIntents = new ArrayList<PendingIntent>();
+			ArrayList<PendingIntent> deliveredPendingIntents = new ArrayList<PendingIntent>();
+
+			for (int i = 0; i < smsBodyParts.size(); i++) {
+				sentPendingIntents.add(sentPendingIntent);
+				deliveredPendingIntents.add(deliveredPendingIntent);
+			}
+
+			// For when the SMS has been sent
+			registerReceiver(new BroadcastReceiver() {
+				@Override
+				public void onReceive(Context context, Intent intent) {
+					switch (getResultCode()) {
+						case Activity.RESULT_OK:
+							Toast.makeText(context, "SMS sent successfully", Toast.LENGTH_SHORT).show();
+							break;
+						case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+							Toast.makeText(context, "Generic failure cause", Toast.LENGTH_SHORT).show();
+							break;
+						case SmsManager.RESULT_ERROR_NO_SERVICE:
+							Toast.makeText(context, "Service is currently unavailable", Toast.LENGTH_SHORT).show();
+							break;
+						case SmsManager.RESULT_ERROR_NULL_PDU:
+							Toast.makeText(context, "No pdu provided", Toast.LENGTH_SHORT).show();
+							break;
+						case SmsManager.RESULT_ERROR_RADIO_OFF:
+							Toast.makeText(context, "Radio was explicitly turned off", Toast.LENGTH_SHORT).show();
+							break;
+					}
+				}
+			}, new IntentFilter(SMS_SENT));
+
+			// For when the SMS has been delivered
+			registerReceiver(new BroadcastReceiver() {
+				@Override
+				public void onReceive(Context context, Intent intent) {
+					switch (getResultCode()) {
+						case Activity.RESULT_OK:
+							Toast.makeText(getBaseContext(), "SMS delivered", Toast.LENGTH_SHORT).show();
+							break;
+						case Activity.RESULT_CANCELED:
+							Toast.makeText(getBaseContext(), "SMS not delivered", Toast.LENGTH_SHORT).show();
+							break;
+					}
+				}
+			}, new IntentFilter(SMS_DELIVERED));
+
+			// Send a text based SMS
+			smsManager.sendMultipartTextMessage("+33608421973", null, smsBodyParts, sentPendingIntents, deliveredPendingIntents);
         }
     };
 
@@ -115,3 +184,4 @@ public class MessagesActivity extends Activity {
         }
     }
 }
+
